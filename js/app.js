b38c3a395842cfea3c2cdfdce5948a61d881a62c
unknown <mahmodulshahin665@gmail.com>
2022-04-02 21:38:04 +0600

    select btn and input

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-alert('js file is added')
+
+const depositBtn = document.getElementById('deposit-btn');
+const buyBtc = document.getElementById('buy-btc');
+const buyEth = document.getElementById('buy-eth');
+const balanceField = document.getElementById('balance-field');
+const btcField = document.getElementById('btc-field');
+const ethField = document.getElementById('eth-field');
+
+depositBtn.addEventListener('click', function (event) {
+    event.preventDefault();
+    let depositBox = document.getElementById('deposit-box');
+    let availabelBalance = parseFloat(depositBox.value);
+    console.log(depositBox.value)
+    balanceField.innerText = depositBox.value;
+    // clear input
+    depositBox.value = '';
+
+})
+
