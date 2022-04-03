51ee1e0fa86776ced2bf6e9be7639cd73b11bb27
unknown <mahmodulshahin665@gmail.com>
2022-04-03 14:14:53 +0600

    btc & eth btn calc

--- js/app.js
+++ js/app.js
@@ -6,14 +6,57 @@ const balanceField = document.getElementById('balance-field');
 const btcField = document.getElementById('btc-field');
 const ethField = document.getElementById('eth-field');
 
+//get value from input box
+function getInputValue(boxId) {
+    let inputBox = document.getElementById(boxId);
+    let inputAmount = parseFloat(inputBox.value);
+    if (isNaN(inputAmount) || inputAmount < 0) {
+        inputBox.value = '';
+        return alert('Please input valid amount of money in number format')
+    }
+    inputBox.value = '';
+    return inputAmount;
+};
+
+//update balance
 depositBtn.addEventListener('click', function (event) {
     event.preventDefault();
-    let depositBox = document.getElementById('deposit-box');
-    let availabelBalance = parseFloat(depositBox.value);
-    console.log(depositBox.value)
-    balanceField.innerText = depositBox.value;
-    // clear input
-    depositBox.value = '';
-
+    let availabelAmount = parseFloat(balanceField.innerText);
+    let inputAmount = getInputValue('deposit-box');
+    if (inputAmount > 0) {
+        balanceField.innerText = inputAmount + availabelAmount;
+    }
 })
+//buy coin
+buyBtc.addEventListener('click', function () {
+    updatePortfolio('btc-box');
+})
+buyEth.addEventListener('click', function () {
+    updatePortfolio('eth-box');
+})
+
+//update portfolio
+function updatePortfolio(boxId) {
+    let availabelAmount = parseFloat(balanceField.innerText);
+    let coinAmount = getInputValue(boxId);
+
+    if (coinAmount > 0) {
+        let totalExpense;
+        if (boxId == 'btc-box') {
+            totalExpense = 10 * coinAmount;
+            if (totalExpense > availabelAmount) {
+                return alert('Not enough balance')
+            }
+            btcField.innerText = coinAmount;
+        } else if (boxId == 'eth-box') {
+            totalExpense = 5 * coinAmount;
+            if (totalExpense > availabelAmount) {
+                return alert('Not enough balance')
+            }
+            ethField.innerText = coinAmount;
+        }
+        balanceField.innerText = availabelAmount - totalExpense;
+    }
+}
+
 
